309231682485ba9accfc9c6806c17e3ff26933a6
sukrit <101797008+Sukrit-garg@users.noreply.github.com>
2026-05-11 23:09:19 +0530

    Time: 165 ms (70.94%) | Memory: 20.6 MB (80.63%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -2,21 +2,16 @@ class Solution:
     def calculateHours(self,speed,piles):
         time = 0
         for i in piles:
-            if i%speed==0:
-                time+=i//speed
-            else:
-                time+=i//speed+1
+            time+=(i+speed-1)//speed
         return time
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         low = 1
         high = max(piles)
-        ans = 0
         while low<=high:
             mid = (low+high)//2
             time = self.calculateHours(mid,piles)
             if time >h:
                 low = mid+1
             else:
-                ans = mid
                 high = mid-1
-        return ans
+        return low
