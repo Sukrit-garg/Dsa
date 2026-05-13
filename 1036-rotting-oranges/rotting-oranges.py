5e79e2e4f918b88424dd92f2618b1fcc5c84715e
sukrit <101797008+Sukrit-garg@users.noreply.github.com>
2026-05-13 19:32:04 +0530

    Time: 3 ms (76.36%) | Memory: 19.3 MB (40.81%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,6 +1,5 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        max_time = 0
         start_indices = []
         rows = len(grid)
         cols = len(grid[0])
@@ -13,31 +12,29 @@ class Solution:
                     good_oranges+=1
         if good_oranges == 0:
             return 0
-        new_grid = copy.deepcopy(grid)
         del_r= [-1,1,0,0]
         del_c= [0,0,-1,1]
         queue = deque(start_indices)
         def bfs(queue):
-            size=len(queue)
-            if size == 0:
-                return
-            nonlocal max_time, good_oranges
-            max_time+=1
-            for i in range(size):
-                row,col = queue.popleft()
-                for j in range(4):
-                    new_r,new_c = row+del_r[j],col+del_c[j]
-                    if 0<=new_r<rows and 0<=new_c<cols and new_grid[new_r][new_c]==1:
-                        new_grid[new_r][new_c]=2
-                        good_oranges-=1
-                        queue.append([new_r,new_c])
-            bfs(queue)
-            return
-        bfs(queue)
-        if good_oranges:
-            return -1
-        else:
-            return max_time - 1
+            nonlocal good_oranges
+            max_time = -1
+            while(queue):
+                max_time+=1
+                size=len(queue)
+                for i in range(size):
+                    row,col = queue.popleft()
+                    for j in range(4):
+                        new_r,new_c = row+del_r[j],col+del_c[j]
+                        if 0<=new_r<rows and 0<=new_c<cols and grid[new_r][new_c]==1:
+                            grid[new_r][new_c]=2
+                            good_oranges-=1
+                            queue.append([new_r,new_c])
+            if good_oranges:
+                return -1
+            else:
+                return max_time
+        return bfs(queue)
+        
         
         
 
